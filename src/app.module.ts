1701d043f376e1aaad620a607fee6b6e45a8c3f5
Amara Bamba <amara.dianney@gmail.com>
2020-02-02 15:46:52 +0000

    configuration of .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+      TypeOrmModule.forRoot(),
+      ConfigModule.forRoot({
+          isGlobal: true,
+          expandVariables: true,
+      })
+  ],
   controllers: [],
   providers: []
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("api/doc", app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server running on http://localhost:${port}`,  'Bootstrap');
 }
 bootstrap();
